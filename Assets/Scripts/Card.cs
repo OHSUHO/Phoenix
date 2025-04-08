b7408e5766dee710c280da26fdfac7db5d103e08
OHSUHO <suho.oh1213@gmail.com>
2025-04-08 14:59:50 +0900

    add : 카드생성, 배치로직 추가, 이미지 파일 수정
    카드생성, 배치로직 추가, 이미지 파일 수정

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour
+{
+    int idx = 0;
+
+    public Animator anim;
+    public GameObject front;
+    public GameObject back;
+    public SpriteRenderer frontImage;
+    
+    private void Start()
+    {
+     
+
+    }
+    public void SelectedCard()
+    {
+        anim.SetBool("isSelected", true);
+        InvokeSetActiveFront();
+        InvokeSetUnActiveBack();
+        Invoke("MissMatched", 1.0f);
+        
+        
+    }
+
+    public void Setting(int num)
+    {
+        idx = num;
+        frontImage.sprite = Resources.Load<Sprite>($"Phoenix{num}");
+    }
+
+    void MissMatched()
+    {
+        anim.SetBool("isSelected", false);
+        InvokeSetUnActiveFront();
+        InvokeSetActiveBack();
+    }
+
+    
+    void InvokeSetActiveFront()
+    {
+        Invoke("SetActiveFront", 0.5f);
+    }
+
+    void SetActiveFront()
+    {
+        front.SetActive(true);
+    }
+
+    void InvokeSetUnActiveFront()
+    {
+        Invoke("SetUnActiveFront", 0.5f);
+    }
+
+    void SetUnActiveFront()
+    {
+        front.SetActive(false);
+    }
+
+    void InvokeSetUnActiveBack()
+    {
+        Invoke("SetUnActiveBack", 0.5f);
+    }
+    void SetUnActiveBack()
+    {
+        back.SetActive(false);
+    }
+
+    void InvokeSetActiveBack()
+    {
+        Invoke("SetActiveBack", 0.5f);
+    }
+    void SetActiveBack()
+    {
+        back.SetActive(true);
+    }
+}
